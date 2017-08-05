f65acd6fde930dba34faaa9f75250ead3dcfcfd8
cookda <daltacook@gmail.com>
2017-08-05 17:06:08 -0400

    It is scraping info!

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace WebScraper
 {
@@ -10,7 +11,14 @@ namespace WebScraper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hey Git");
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=entry%20level%20software%20engineer&sc.keyword=entry%20level%20software%20engineer&locT=&locId=&jobType=&jl=2474142853");
+            var HeaderNames = doc.DocumentNode.SelectNodes("//a[@class='jobLink']").ToList();
+            foreach (var item in HeaderNames)
+            {
+                Console.WriteLine(item.InnerText);
+            }
+
         }
     }
 }
